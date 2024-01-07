0a009657122dbb7d783dc973c79b54fd6cce33cb
kimyoungjae <jae2942@gmail.com>
2024-01-07 21:57:56 +0900

    CHORE : DM user gravatar & nickname add

--- front/pages/DirectMessage/index.tsx
+++ front/pages/DirectMessage/index.tsx
@@ -9,15 +9,17 @@ import { useParams } from 'react-router'
 const DirectMessage = () => {
 
   const {workspace,id}=useParams<{workspace:string,id:string}>()
-  const {data:userData} = useSWR(`/api/workspace/${workspace}/members/${id}`,fetcher)
-
+  const {data:userData} = useSWR(`/api/workspaces/${workspace}/users/${id}`,fetcher)
+  const {data:myData} = useSWR(`/api/users`,fetcher)
+  if(!userData ||!myData){return null}
   return (
     <Container>
       <Header>
         <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.nickname} />
         <span>{userData.nickname}</span>
       </Header>
-
+      {/* <ChatList></ChatList>
+      <ChatBox></ChatBox> */}
       <DragOver>업로드!</DragOver>
     </Container>
      
