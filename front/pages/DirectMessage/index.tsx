8b5ceb312a6525862652118c9d7be572343bf76a
kimyoungjae <jae2942@gmail.com>
2024-01-07 21:47:08 +0900

    FEAT :DM message page add

--- front/pages/DirectMessage/index.tsx
+++ front/pages/DirectMessage/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import Workspace from '@layouts/Workspace'
 import { Container } from './styles'
+import gravatar from 'gravatar'
+import { Header,DragOver } from './styles'
+import useSWR from 'swr'
+import fetcher from '@utils/fetcher'
 const DirectMessage = () => {
-    
+  
+    const {data:userData} = useSWR('/api/users',fetcher)
+
   return (
-    <Container>DM</Container>
+    <Container>
+      <Header>
+        <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.nickname} />
+        <span>{userData.nickname}</span>
+      </Header>
+
+      <DragOver>업로드!</DragOver>
+    </Container>
      
   )
 }
