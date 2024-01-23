09a5ecd4fd1865b541f27078488bdd1ea1f1a6c1
kim young jae <jae2942@gmail.com>
2024-01-23 18:39:00 +0900

    FIX : scrollbar bottom delay fixed

--- front/pages/DirectMessage/index.tsx
+++ front/pages/DirectMessage/index.tsx
@@ -26,8 +26,21 @@ const DirectMessage = () => {
   const [chat,onChangeChat,setChat] =useInput('')
   const  scrollRef = useRef<Scrollbars>(null)
   const onSubmitForm = useCallback((e:any)=>{
-    e.preventDefault();
-    if(chat?.trim()){
+    e.preventDefault(); 
+    if(chat?.trim() && chatData){
+      const savedChat = chat;
+      mutateChat((prevChatData)=>{
+        prevChatData?.[0].unshift({
+          id:(chatData[0][0]?.id||0)+1,
+          content:savedChat,
+          SenderId:myData.id,
+          Sender:myData,
+          ReceiverId:userData.id,
+          Receiver:userData,
+          createdAt:new Date(),
+        })  
+        return prevChatData;
+      })
    
       axios.post(`/api/workspaces/${workspace}/dms/${id}/chats`,{content:chat})
       .then((res:any)=>{setChat(''),mutateChat(),scrollRef.current?.scrollToBottom()})
