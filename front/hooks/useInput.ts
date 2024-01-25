eb007ee4ceeb726affdb6d5d472e8f94e375a2af
kimyoungjae <jae2942@gmail.com>
2024-01-25 21:10:06 +0900

    FIX : login e type modify

--- front/hooks/useInput.ts
+++ front/hooks/useInput.ts
@@ -1,4 +1,4 @@
-import React,{useState,useCallback,Dispatch,SetStateAction} from 'react'
+import React,{useState,useCallback,Dispatch,SetStateAction, ChangeEvent} from 'react'
 
 type Handler = (e:any) => void;
 type ReturnTypes<T = any> = [T, Handler, Dispatch<SetStateAction<T>>];
@@ -6,8 +6,8 @@ type ReturnTypes<T = any> = [T, Handler, Dispatch<SetStateAction<T>>];
 
 const useinput =<T= any> (initialData:T):ReturnTypes<T> => {
     const [value,setValue] = useState(initialData);
-    const handler = useCallback((e:any)=>{
-            setValue(e.target.value)
+    const handler = useCallback((e:ChangeEvent<HTMLInputElement>)=>{
+            setValue(e.target.value as unknown as T)
     },[]);
     return [value,handler,setValue];
 }
